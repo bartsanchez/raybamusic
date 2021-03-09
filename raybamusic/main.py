f7033ba93b190357f91717d9ce1dfe34e064cec3
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-03-09 23:22:52 +0100

    Move to Graphql server using Graphene

--- raybamusic/main.py
+++ raybamusic/main.py
@@ -1,7 +1,14 @@
+import graphene
 from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return "Hello " + name
+
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+schema = graphene.Schema(query=Query)
+app.add_route("/", GraphQLApp(schema=schema))
