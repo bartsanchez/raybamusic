b8061835968f39170588b81c527dce53165d042b
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-10-17 15:04:31 +0200

    NEW: Stop using Relay with Graphene for GraphQL

--- backend/raybamusic/main.py
+++ backend/raybamusic/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 
-from . import models
-from .database import engine
+from . import crud, models
+from .database import engine, SessionLocal
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,12 +13,14 @@ models.Base.metadata.create_all(bind=engine)
 class Song(SQLAlchemyObjectType):
     class Meta:
         model = models.Song
-        interfaces = (graphene.relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    node = graphene.relay.Node.Field()
-    all_songs = SQLAlchemyConnectionField(Song.connection)
+    songs = graphene.List(Song)
+
+    def resolve_songs(self, info):
+        session = info.context.get("session", SessionLocal)
+        return crud.get_all_songs(session)
 
 
 app = FastAPI()
